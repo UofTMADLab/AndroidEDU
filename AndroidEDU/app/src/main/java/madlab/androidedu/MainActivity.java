ba0d3fe6234a1b178af8ee11794e326eb4d78923
aldaliu <alda.liu@mail.utoronto.ca>
2016-01-11 16:50:33 -0500

    Added lines to show changing layout elements through code

--- AndroidEDU/app/src/main/java/madlab/androidedu/MainActivity.java
+++ AndroidEDU/app/src/main/java/madlab/androidedu/MainActivity.java
@@ -2,14 +2,41 @@ package madlab.androidedu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int test;
+    private TextView timer;
+    private Button startButton;
+    private Button pauseButton;
+    private Button restartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timer = (TextView) findViewById(R.id.timer);
+        startButton = (Button) findViewById(R.id.start_button);
+        pauseButton = (Button) findViewById(R.id.pause_button);
+        restartButton = (Button) findViewById(R.id.restart_button);
+
+        timer.setText("This is the timer");
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "This is a toast", Toast.LENGTH_SHORT).show();
+            }
+        });
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.setText("00:00:00");
+            }
+        });
+
     }
 }
