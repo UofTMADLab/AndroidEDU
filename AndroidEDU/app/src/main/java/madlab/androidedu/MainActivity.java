96d069471b3ab8ce16921deeaa737b3ff96ea503
aldaliu <alda.liu@mail.utoronto.ca>
2016-01-19 14:44:02 -0500

    Added comments

--- AndroidEDU/app/src/main/java/madlab/androidedu/MainActivity.java
+++ AndroidEDU/app/src/main/java/madlab/androidedu/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int test;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
